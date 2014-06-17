b51885a38da11e4aacb033c08a4f75711d428058
Aram Zucker-Scharff <azuckers@gmu.edu>
2014-06-17 16:43:21 -0400

    a living clone. Hello Dolly.

--- assets/js/wpagatt-admin.js
+++ assets/js/wpagatt-admin.js
@@ -1,35 +1,35 @@
-var elementCounter = 0;
+var elementCounter = 1;
     jQuery(document).ready(function() {           
         jQuery(".add-repeater").click(function() {
             var parent  =   jQuery(this).parent('.repeater-container');
             var parentID =  parent.attr('id');
             var elementID = parent.attr('for');
-            var elementRow = jQuery(parentID + " #" + elementID).clone();
+            var elementRow = jQuery('#'+parentID + " #" + elementID).clone();
             var newID = elementID + "-" + elementCounter;
             console.log(elementRow);    
             elementRow.attr("id", newID);
             elementRow.show();
             
             
-            jQuery('#' + newID + ' input').each(function (index) {
+            jQuery('input', elementRow).each(function (index) {
                 
-                var input_index_num_begins_at = this.attr("name").indexOf("element-num-");
-                var input_stringStarts = this.attr("name").substring(0,index_num_begins_at);
-                var input_stringEnds = this.attr("class");
-                var input_stringEnds_complete = "["+stringEnds+"]";
-                var input_string = stringStarts + "element-num-" + elementCounter +"]"+stringEnds_complete;
-                this.attr("name", input_string); 
+                var input_index_num_begins_at = jQuery(this).attr("name").indexOf("element-num-");
+                var input_stringStarts = jQuery(this).attr("name").substring(0,input_index_num_begins_at);
+                var input_stringEnds = jQuery(this).attr("class");
+                var input_stringEnds_complete = "["+input_stringEnds+"]";
+                var input_string = input_stringStarts + "element-num-" + elementCounter +"]"+input_stringEnds_complete;
+                jQuery(this).attr("name", input_string); 
                  
             });
                                                 
-            jQuery('#' + newID + ' label').each(function (index) {
+            jQuery('label', elementRow).each(function (index) {
                 
-                var input_index_num_begins_at = this.attr("for").indexOf("element-num-");
-                var input_stringStarts = this.attr("for").substring(0,index_num_begins_at);
-                var input_stringEnds = this.attr("class");
-                var input_stringEnds_complete = "["+stringEnds+"]";
-                var input_string = stringStarts + "element-num-" + elementCounter +"]"+stringEnds_complete;
-                this.attr("for", input_string); 
+                var label_index_num_begins_at = jQuery(this).attr("for").indexOf("element-num-");
+                var label_stringStarts = jQuery(this).attr("for").substring(0,label_index_num_begins_at);
+                var label_stringEnds = jQuery(this).attr("class");
+                var label_stringEnds_complete = "["+label_stringEnds+"]";
+                var label_string = label_stringStarts + "element-num-" + elementCounter +"]"+label_stringEnds_complete;
+                jQuery(this).attr("for", label_string); 
                  
             });
  
